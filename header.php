30939dd6f3f0ed37ff209ba456c4818c21cd1650
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-28 20:43:03 -0600

    Agregar markup para h-card

--- header.php
+++ header.php
@@ -219,7 +219,7 @@
                 <div class="col-12 col-md-10 offset-md-1">
                     <div class="row">
                         <div class="col-12 col-md-3 col-lg-2 d-flex align-items-center mb-3 mb-md-0">
-                            <div class="mt-3 mb-1" id="logo">
+                            <div class="mt-3 mb-1 u-photo" id="logo">
                                 <a class="h-card" href="https://<?php include('includes/site-domain.php'); ?>/" data-toggle="tooltip" data-placement="right" title="" data-original-title="Sígueme en Mastodon: <?php include('includes/mastodon-account.php'); ?>">
                                     <span>Logo</span>
                                 </a>
