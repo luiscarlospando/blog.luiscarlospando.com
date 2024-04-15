2aacdfa8b0d45b485e825d66c290536ec4592db1
Luis Carlos Pando <hey@luiscarlospando.com>
2024-04-15 10:16:00 -0600

    Armar componente de alerta de transmisión en vivo

--- header.php
+++ header.php
@@ -233,35 +233,4 @@
             </div>
         </header>
 
-        <section id="mijostreams-livestream-alert" class="container">
-            <div class="row">
-                <div class="col-12 col-md-10 offset-md-1">
-                    <div class="card last-updated text-center mt-3">
-                        <div class="card-body rounded">
-                            <p>
-                                <i id="live-icon" class="fa-solid fa-circle animated live infinite"></i>
-                                ¡Estoy <a href="https://<?php include('includes/site-domain.php'); ?>/live/">en vivo</a>!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section id="m7gp-livestream-alert" class="container">
-            <div class="row">
-                <div class="col-12 col-md-10 offset-md-1">
-                    <div class="card last-updated text-center mt-3">
-                        <div class="card-body rounded">
-                            <p>
-                                <i id="live-icon" class="fa-solid fa-circle animated live infinite"></i>
-                                ¡El <a href="https://<?php include('includes/site-domain.php'); ?>/games/mario-kart/">Mode 7 Grand Prix</a> está en vivo!
-                            </p>
-                            <p>
-                                Código del torneo: <code>0746-6549-8155</code>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <section id="live" class="container"></section>
