b862a70064774f090c5d9aef10d4078e8a71f47e
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-17 17:46:39 -0600

    Mejorar semántica

--- loop-fotos.php
+++ loop-fotos.php
@@ -3,11 +3,26 @@
         the_post(); ?>
 
         <div class="col-md-4 mb-4">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail("thumb-foto", [
-                    "class" => "thumb-foto rounded img-fluid",
-                ]); ?>
-            </a>
+            <figure class="figure">
+                <a href="<?php the_permalink(); ?>"
+                   data-toggle="tooltip"
+                   data-placement="top"
+                   title="<?php echo esc_attr(get_the_title()); ?>"
+                   data-original-title="<?php echo esc_attr(
+                       get_the_title()
+                   ); ?>">
+
+                    <?php the_post_thumbnail("thumb-foto", [
+                        "class" => "thumb-foto rounded img-fluid",
+                        "alt" => get_the_title(),
+                        "title" => get_the_title(),
+                        "caption" => get_the_title(),
+                    ]); ?>
+                </a>
+                <figcaption class="figure-caption text-center">
+                    <?php echo get_the_title(); ?>
+                </figcaption>
+            </figure>
         </div>
 <?php
     endwhile; ?>
