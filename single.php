af4ec54095e6690ddc7a377ad8047d3c97e4144e
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-17 14:27:32 -0600

    Cambiar color de los hashtags

--- single.php
+++ single.php
@@ -59,7 +59,7 @@
              echo '<em><span class="meta-sep"></span></em>';
              foreach ($tags as $tag) {
                  $sep = $tag === end($tags) ? "" : " ";
-                 echo '<a class="badge badge-dark" href="' .
+                 echo '<a class="badge badge-secondary" href="' .
                      get_term_link($tag, $tag->taxonomy) .
                      '">#' .
                      $tag->name .
