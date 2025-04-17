c6925db964b04fbaac0e6017576b89692c4988d8
Luis Carlos Pando <hey@luiscarlospando.com>
2025-04-17 14:31:52 -0600

    Cambiar color de los hashtags

--- tag.php
+++ tag.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-12 col-md-10 offset-md-1">
 				<h1 class="text-center">
-                    <span class="badge badge-dark">
+                    <span class="badge badge-primary">
                         <?php
                         _e("", "html5blank");
                         echo single_tag_title("#", false);
