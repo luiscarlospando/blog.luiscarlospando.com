a2c9f49dea1a0144089e3f960caff8ebffb7359b
Luis Carlos Pando <hey@luiscarlospando.com>
2025-05-09 09:02:17 -0600

    Actualizar disclaimer de los comentarios

--- includes/comments.php
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-        repo="luiscarlospando/comments"
-        issue-term="title"
-        label="Comment"
-        theme="github-dark-orange"
-        crossorigin="anonymous"
-        async>
-</script>
