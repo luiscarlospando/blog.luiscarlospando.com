343123a1d9d359247cb06692958929ea68605e9f
Luis Carlos Pando <hey@luiscarlospando.com>
2025-05-10 03:52:41 -0600

    Agregar botón para dejar comentario

---
+++ includes/fetched-toot-reply-button.php
@@ -0,0 +1,17 @@
+<?php
+$post_id = get_the_ID();
+$toot_url = get_post_meta($post_id, "_mastodon_toot_url", true);
+
+if (!$toot_url) {
+    force_check_mastodon_toot($post_id);
+    $toot_url = get_post_meta($post_id, "_mastodon_toot_url", true);
+}
+
+if ($toot_url) {
+    echo '<li class="list-inline-item"><a class="btn btn-primary btn-sm" id="btn-mastodon-toot" href="' .
+        esc_url($toot_url) .
+        '" target="_blank"><i class="fa-solid fa-reply"></i> Enviar comentario</a></li>';
+} else {
+    // For posts without a corresponding Mastodon toot
+    echo "";
+}
