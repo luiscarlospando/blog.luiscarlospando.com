77add43aa7235f395aee13453445e382082cfe2c
Luis Carlos Pando <hey@luiscarlospando.com>
2025-08-05 15:22:24 -0600

    Agregar más opciones para retener lectores

--- single.php
+++ single.php
@@ -97,7 +97,7 @@
 					</ul>
 
 					<div class="comments-disclaimer mb-4">
-						<?php include "includes/fetched-toot-link.php"; ?>. Debes de tener una cuenta de <a href="<?php include "includes/mastodon-account.php"; ?>" rel="me" target="_blank"><i class="fa-brands fa-mastodon"></i> Mastodon</a>, las razones las explico <a href="https://blog.luiscarlospando.com/coding/2023/02/hay-nuevo-sistema-de-comentarios-en-mi-blog/">aquí</a>. También, si le das <em>like</em> o le das <em>boost</em> a este post en Mastodon, aparecerás aquí debajo. Si no tienes cuenta y te gustaría decir algo, checa mi <a href="https://luiscarlospando.com/contacto"><i class="fa-solid fa-address-card"></i> página de contacto</a> o contáctame vía <a href="https://discordapp.com/users/86571896581132288/" rel="me" target="_blank"><i class="fa-brands fa-discord"></i> Discord</a>.
+						<?php include "includes/fetched-toot-link.php"; ?>. Debes de tener una cuenta de <a href="<?php include "includes/mastodon-account.php"; ?>" rel="me" target="_blank"><i class="fa-brands fa-mastodon"></i> Mastodon</a>, las razones las explico <a href="https://blog.<?php include "includes/site-domain.php"; ?>/coding/2023/02/hay-nuevo-sistema-de-comentarios-en-mi-blog/">aquí</a>. También, si le das <em>like</em> o le das <em>boost</em> a este post en Mastodon, aparecerás aquí debajo. Si no tienes cuenta y te gustaría decir algo, checa mi <a href="https://luiscarlospando.com/contacto"><i class="fa-solid fa-address-card"></i> página de contacto</a> o contáctame vía <a href="https://discordapp.com/users/86571896581132288/" rel="me" target="_blank"><i class="fa-brands fa-discord"></i> Discord</a>.
 					</div>
 
 					<div id="webmentions-comments-subtitle"></div>
@@ -110,6 +110,7 @@
 
 					<div class="share-post card text-center mb-4">
                        	<div class="card-body">
+                            Si es así, pues te invito a seguir leyendo más posts del <a class="btn btn-primary btn-sm" href="https://blog.<?php include "includes/site-domain.php"; ?>/"><i class="fa-solid fa-file-pen"></i> Blog</a>, o tal vez quieras saber qué está pasando conmigo, entonces checa el <a class="btn btn-primary btn-sm" href="https://<?php include "includes/site-domain.php"; ?>/now"><i class="fa-solid fa-clock"></i> Now</a> o ¿por qué no pasas a firmar mi <a class="btn btn-primary btn-sm" href="https://<?php include "includes/site-domain.php"; ?>/guestbook"><i class="fa-solid fa-file-signature"></i> Guestbook</a>?
                             Puedes apoyarme
                             <a class="btn btn-primary btn-sm" href="https://buymeacoffee.com/luiscarlospando" target="_blank">
                                 <i class="fa-solid fa-mug-hot"></i> Comprándome un café
