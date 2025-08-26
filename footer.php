eead6392c5d81f3dca199992f4cf6e7d4cacf517
Luis Carlos Pando <hey@luiscarlospando.com>
2025-08-26 11:05:03 -0600

    Centrar contenido de modales

--- footer.php
+++ footer.php
@@ -403,7 +403,7 @@
 
     <!-- Modal -->
     <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-lg modal-dialog-centered text-center" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
