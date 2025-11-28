b4fa274f8c9f0d843801ebd3559c81801a8d9bdc
Luis Carlos Pando <hey@luiscarlospando.com>
2025-11-28 13:39:14 -0600

    Agregar botón de suscripción por RSS

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 			<div class="col-12 col-md-10 offset-md-1">
 				<h1 class="text-center"><?php _e("Blog", "html5blank"); ?></h1>
 
+				<ul class="list-inline mb-4 text-center">
+                    <li class="list-inline-item">
+                        <a class="btn btn-primary btn-sm" href="https://blog.luiscarlospando.com/rss/" rel="me noopener">
+                            <i class="fa-solid fa-rss"></i> Suscribirse
+                        </a>
+                    </li>
+                </ul>
+
 				<?php get_template_part("loop"); ?>
 
 				<?php get_template_part("pagination"); ?>
