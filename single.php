a570d4d170c46578520814f6518732e498a29fd6
Luis Carlos Pando <hey@luiscarlospando.com>
2026-08-05 09:12:54 -0600

    Reestructurar CTAs

--- single.php
+++ single.php
@@ -129,9 +129,7 @@
 
 					<div class="share-post card text-center mb-4">
                        	<div class="card-body">
-                            <p>Lee más en el <a class="btn btn-primary btn-sm" href="https://blog.<?php include "template-parts/site-domain.php"; ?>/"><i class="fa-solid fa-message"></i> Blog</a>,revisa en qué ando <a class="btn btn-primary btn-sm" href="https://<?php include "template-parts/site-domain.php"; ?>/now/"><i class="fa-solid fa-clock"></i> Now</a>, o déjame un saludos en el <a class="btn btn-primary btn-sm" href="https://<?php include "template-parts/site-domain.php"; ?>/guestbook/"><i class="fa-solid fa-pen-nib"></i> Guestbook</a>.</p>
-
-                            <br>
+                            <p>Lee más en el <a class="btn btn-primary btn-sm" href="https://blog.<?php include "template-parts/site-domain.php"; ?>/"><i class="fa-solid fa-message"></i> Blog</a>, revisa en qué ando <a class="btn btn-primary btn-sm" href="https://<?php include "template-parts/site-domain.php"; ?>/now/"><i class="fa-solid fa-clock"></i> Now</a>, o déjame un saludo en el <a class="btn btn-primary btn-sm" href="https://<?php include "template-parts/site-domain.php"; ?>/guestbook/"><i class="fa-solid fa-pen-nib"></i> Guestbook</a>.</p>
 
                             <ul class="list-inline mb-0">
                                 <li class="list-inline-item">
